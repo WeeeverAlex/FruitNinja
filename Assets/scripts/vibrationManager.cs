f2ba8292b37352d189a608114e46a3ac0558f459
Eduardosmb <101536778+Eduardosmb@users.noreply.github.com>
2024-05-28 21:20:44 -0300

    .

--- Assets/scripts/vibrationManager.cs
+++ Assets/scripts/vibrationManager.cs
@@ -1,17 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class VibrationManager : MonoBehaviour
 {
-    public static void Vibrate(long milliseconds)
+    public static void Vibrate(long milliseconds, int amplitude = -1)
     {
         if (Application.platform == RuntimePlatform.Android)
         {
             AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
             AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
-            vibrator.Call("vibrate", milliseconds);
+
+            if (amplitude != -1)
+            {
+                AndroidJavaClass vibrationEffectClass = new AndroidJavaClass("android.os.VibrationEffect");
+                AndroidJavaObject vibrationEffect = vibrationEffectClass.CallStatic<AndroidJavaObject>("createOneShot", milliseconds, amplitude);
+                vibrator.Call("vibrate", vibrationEffect);
+            }
+            else
+            {
+                vibrator.Call("vibrate", milliseconds);
+            }
         }
         else
         {
